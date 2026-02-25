08ec5539c4235c0a77a1f031cce95cef6dceb8ad
Aleksey Lozhkin <aleksey.s.lozhkin@gmail.com>
2026-02-25 06:05:07 +0300

    Realized task 3

--- materials/serializers.py
+++ materials/serializers.py
@@ -3,29 +3,6 @@ from rest_framework import serializers
 from .models import Course, Lesson
 
 
-class CourseSerializer(serializers.ModelSerializer):
-    """Сериализатор для курса"""
-    lessons_count = serializers.SerializerMethodField()
-
-    class Meta:
-        model = Course
-        fields = [
-            'id',
-            'title',
-            'preview',
-            'description',
-            'owner',
-            'created_at',
-            'updated_at',
-            'lessons_count'
-        ]
-        read_only_fields = ['id', 'created_at', 'updated_at']
-
-    def get_lessons_count(self, obj):
-        """Возвращает количество уроков в курсе"""
-        return obj.lessons.count()
-
-
 class LessonSerializer(serializers.ModelSerializer):
     """Сериализатор для урока"""
 
@@ -45,3 +22,35 @@ class LessonSerializer(serializers.ModelSerializer):
             'updated_at',
         ]
         read_only_fields = ['id', 'created_at', 'updated_at']
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    """Сериализатор для курса"""
+
+    # Поле для количества уроков
+    lessons_count = serializers.SerializerMethodField()
+
+    # Поле для списка всех уроков курса
+    lessons = LessonSerializer(many=True, read_only=True, source='lessons.all')
+
+    class Meta:
+        model = Course
+        fields = [
+            'id',
+            'title',
+            'preview',
+            'description',
+            'owner',
+            'created_at',
+            'updated_at',
+            'lessons_count',
+            'lessons'
+        ]
+        read_only_fields = ['id', 'created_at', 'updated_at']
+
+    def get_lessons_count(self, obj):
+        """Возвращает количество уроков в курсе"""
+        return obj.lessons.count()
+
+
+
